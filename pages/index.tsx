774f597c37ca96cd8ec1c5e890574efd976ff6d5
arad taghikhani <arad5@outlook.com>
2022-02-11 22:47:56 -0800

    initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+      <Head>
+        <title>Arad</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    
+    </div>
+  )
+}
